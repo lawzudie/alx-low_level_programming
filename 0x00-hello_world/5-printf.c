6a9f07e373e5de1d019fe560798a981a9ee4fb7b
Lawrence Udie <lawzudie@gmail.com>
2022-12-08 20:42:49 -0800

    5printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - starting point of program
- * return - zero
+ * Return - this should return zero.
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }

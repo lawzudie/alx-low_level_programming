e059b08c43ea922a4068efb02d28132baa10b6c5
Lawrence Udie <lawzudie@gmail.com>
2022-12-12 19:17:05 -0800

    6 task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 48; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

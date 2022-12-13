c1b53b9a3e12d25c35c95eb784bfc464aa86a035
Lawrence Udie <lawzudie@gmail.com>
2022-12-12 18:59:26 -0800

    4 tsk

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase except e and q
+ * Description - followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

6564b1fed6f5bc9d2cb9d7163fdc9e4317afd325
Lawrence Udie <lawzudie@gmail.com>
2022-12-14 04:33:20 -0800

    3 task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - this function checks lower case character
+ *
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

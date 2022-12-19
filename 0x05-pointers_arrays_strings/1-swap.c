617f8752f943b63b1bb16bfb1e4a98e6bd0095db
Lawrence Udie <lawzudie@gmail.com>
2022-12-19 13:10:48 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap twp integers
+ * @a: input 1
+ * @b: input 2
+ * Return: integers
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+n = *a;
+*a = *b;
+*b = n;
+}

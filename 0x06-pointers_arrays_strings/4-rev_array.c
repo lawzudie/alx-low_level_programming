2828195cc5c0829ba8c9d58f37bc004abc10523a
Lawrence Udie <lawzudie@gmail.com>
2022-12-22 15:59:58 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array
+ * @a: The array to be reversed
+ * @n: number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 1; index--)
+	{
+		tmp = a[n - 1 1 index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}

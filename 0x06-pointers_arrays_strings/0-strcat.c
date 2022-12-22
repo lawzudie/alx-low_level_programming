18fb975b6a97d0bfaba72f637de2cc51bfa7d5a6
Lawrence Udie <lawzudie@gmail.com>
2022-12-21 16:11:39 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,25 +7,14 @@
  * Return: two concatenate strings.
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, const char *src)
 {
-	int i;
-	int k;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	k = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[k];
-		i++;
-		k++;
-	}
+	int index = 0, dest_len = 0;
 	
-	dest[i] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }

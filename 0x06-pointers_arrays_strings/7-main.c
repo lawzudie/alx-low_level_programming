c0230325707a35e2023fc84d2d8d3c438ab34dbc
Lawrence Udie <lawzudie@gmail.com>
2022-12-22 18:31:07 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for worst, Capitalize what comes.\n";
+	char s[] = "Expect the best. Prepare for the worst, Capitalize on what comes.\n";
 	char *p;
 
 	p = leet(s);
 	printf("%s", p);
 	printf("%s", s);
+
 	return (0);
 }
